713d3d3b7e99c3c9842976e152e4c6908101539b
Kyle Tadashi <devgru0114@gmail.com>
2022-08-07 16:23:55 -0400

    swiper

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <swiper
+    direction="vertical"
+    :slidesPerView="2"
+    :spaceBetween="30"
+    :mousewheel="true"
+    :pagination="{
+      clickable: true,
+    }"
+    :modules="modules"
+  >
+    <swiper-slide
+      v-for="i in 9"
+      :key="`staked-${i}`">
+      Slide {{i}}
+    </swiper-slide>
+  </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+
+import "./style.css";
+
+// import required modules
+import { Mousewheel, Pagination } from "swiper";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Mousewheel, Pagination],
+    };
+  },
+};
+</script>
